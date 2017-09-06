e8460bb41e6dc5d36ee128a60d44c6e42b59702c
felipemlopes <felipemarcanthlopes@gmail.com>
2017-09-06 15:35:37 -0300

    teste

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use App\Conversations\ExampleConversation;
 use Illuminate\Http\Request;
 use Mpociot\BotMan\BotMan;
+use Mpociot\BotMan\Answer;
+use Mpociot\BotMan\Button;
+use Mpociot\BotMan\Conversation;
+use Mpociot\BotMan\Question;
+use Illuminate\Foundation\Inspiring;
 
 class BotManController extends Controller
 {
@@ -17,11 +22,33 @@ class BotManController extends Controller
         $botman->verifyServices(env('TOKEN_VERIFY'));
 
         // Simple respond method
-        $botman->hears('Hello', function (BotMan $bot) {
+        /*$botman->hears('Hello', function (BotMan $bot) {
             $bot->reply('Hi there :)');
+        });*/
+        $botman->fallback(function($bot) {
+            $bot->reply('Sorry, I did not understand these commands. Here is a list of commands I understand: ...');
         });
 
         $botman->listen();
+
+        /*$question = Question::create("Huh - you woke me up. What do you need?")
+            ->fallback('Unable to ask question')
+            ->callbackId('ask_reason')
+            ->addButtons([
+                Button::create('Tell a joke')->value('joke'),
+                Button::create('Give me a fancy quote')->value('quote'),
+            ]);
+
+        return $this->ask($question, function (Answer $answer) {
+            if ($answer->isInteractiveMessageReply()) {
+                if ($answer->getValue() === 'joke') {
+                    $joke = json_decode(file_get_contents('http://api.icndb.com/jokes/random'));
+                    $this->say($joke->value->joke);
+                } else {
+                    $this->say(Inspiring::quote());
+                }
+            }
+        });*/
     }
 
     /**
